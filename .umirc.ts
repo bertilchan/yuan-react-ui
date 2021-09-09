94136a624870aa107122bdaf6db1b9eaef0feb88
相北 <xiangbei.yzx@alibaba-inc.com>
2021-09-09 16:53:40 +0800

    Add gulp dependencies

--- .umirc.ts
+++ .umirc.ts
@@ -1,22 +1,11 @@
 import {defineConfig} from 'dumi';
 
-let base = '/yuan-react-ui';
-let publicPath = '/yuan-react-ui/';
-
-if (process.env.SITE_BUILD_ENV === 'PREVIEW') {
-    base = '/';
-    publicPath = '/';
-}
-
-base = '/';
-publicPath = '/';
-
 export default defineConfig({
     title: 'YUAN REACT UI', // 站点名称
     mode: 'site',
     outputPath: 'doc-site', // 输出文件夹
     exportStatic: {}, // 后续会部署到 github pages 直接全部生成静态页面 不走前端路由
     dynamicImport: {}, // 拆包 站点过大时可以优化首屏加载速度
-    base,
-    publicPath,
+    base: '/',
+    publicPath: '/',
 });
