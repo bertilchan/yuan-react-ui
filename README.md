3d9c0996d2dba478300566b330825f328e1a48fc
相北 <xiangbei.yzx@alibaba-inc.com>
2021-09-09 15:35:26 +0800

    Finish api doc

---
+++ README.md
@@ -0,0 +1,3 @@
+# 参考文章
+
+[本项目参考该项目搭建](https://github.com/worldzhao/blog/issues)
--- src/alert/index.tsx
+++ src/alert/index.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import t from 'prop-types';
 
 export interface AlertProps {
+    /**
+     * @description       Alert 的类型
+     * @default           'info'
+     */
     kind?: 'info' | 'positive' | 'negative' | 'warning';
 }
 
@@ -16,7 +20,7 @@ const kinds: KindMap = {
     warning: '#FFA502',
 };
 
-const Alert: React.FC<AlertProps> = ({ children, kind = 'info', ...rest }) => (
+const Alert: React.FC<AlertProps> = ({children, kind = 'info', ...rest}) => (
     <div
         className={prefixCls}
         style={{
