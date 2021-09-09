180a7ebadda9c3258066ecc7df76ed51c8bf3d16
相北 <xiangbei.yzx@alibaba-inc.com>
2021-09-09 17:37:44 +0800

    Modify gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -7,16 +7,12 @@ const through2 = require('through2');
 
 const paths = {
     dest: {
-        lib: 'lib', // commonjs 文件存放的目录名 - 本块关注
-        esm: 'esm', // ES module 文件存放的目录名 - 暂时不关心
-        dist: 'dist', // umd文件存放的目录名 - 暂时不关心
+        lib: 'lib',
+        esm: 'esm',
+        dist: 'dist',
     },
-    styles: 'src/**/*.less', // 样式文件路径 - 暂时不关心
-    scripts: [
-        'src/**/*.{ts,tsx}',
-        '!src/**/demo/*.{ts,tsx}',
-        '!src/**/__tests__/*.{ts,tsx}',
-    ], // 脚本文件路径
+    styles: 'src/**/*.less',
+    scripts: ['src/**/*.{ts,tsx}', '!src/**/demo/*.{ts,tsx}', '!src/**/__tests__/*.{ts,tsx}'],
 };
 
 /**
@@ -34,22 +30,22 @@ function cssInjection(content) {
 
 /**
  * 编译脚本文件
- * @param {*} babelEnv babel环境变量
- * @param {*} destDir 目标目录
+ * @param {string} babelEnv babel环境变量
+ * @param {string} destDir 目标目录
  */
 function compileScripts(babelEnv, destDir) {
-    const {scripts} = paths;
+    const { scripts } = paths;
     process.env.BABEL_ENV = babelEnv;
     return gulp
     .src(scripts)
-    .pipe(babel()) // 使用 gulp-babel 处理
+    .pipe(babel()) // 使用gulp-babel处理
     .pipe(
         through2.obj(function z(file, encoding, next) {
             this.push(file.clone());
             // 找到目标
             if (file.path.match(/(\/|\\)style(\/|\\)index\.js/)) {
                 const content = file.contents.toString(encoding);
-                file.contents = Buffer.from(cssInjection(content)); // 文件内容处理
+                file.contents = Buffer.from(cssInjection(content)); // 处理文件内容
                 file.path = file.path.replace(/index\.js/, 'css.js'); // 文件重命名
                 this.push(file); // 新增该文件
                 next();
@@ -65,7 +61,7 @@ function compileScripts(babelEnv, destDir) {
  * 编译cjs
  */
 function compileCJS() {
-    const {dest} = paths;
+    const { dest } = paths;
     return compileScripts('cjs', dest.lib);
 }
 
@@ -73,19 +69,17 @@ function compileCJS() {
  * 编译esm
  */
 function compileESM() {
-    const {dest} = paths;
+    const { dest } = paths;
     return compileScripts('esm', dest.esm);
 }
 
-// 串行执行编译脚本任务（cjs,esm） 避免环境变量影响
 const buildScripts = gulp.series(compileCJS, compileESM);
 
 /**
  * 拷贝less文件
  */
 function copyLess() {
-    return gulp.src(paths.styles).pipe(gulp.dest(paths.dest.lib)).pipe(
-        gulp.dest(paths.dest.esm));
+    return gulp.src(paths.styles).pipe(gulp.dest(paths.dest.lib)).pipe(gulp.dest(paths.dest.esm));
 }
 
 /**
@@ -96,7 +90,7 @@ function less2css() {
     .src(paths.styles)
     .pipe(less()) // 处理less文件
     .pipe(autoprefixer()) // 根据browserslistrc增加前缀
-    .pipe(cssnano({zindex: false, reduceIdents: false})) // 压缩
+    .pipe(cssnano({ zindex: false, reduceIdents: false })) // 压缩
     .pipe(gulp.dest(paths.dest.lib))
     .pipe(gulp.dest(paths.dest.esm));
 }
