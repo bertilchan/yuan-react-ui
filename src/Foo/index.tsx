868babc7b16098ac51998d3d4f7b2ddb8d3cd1dd
相北 <xiangbei.yzx@alibaba-inc.com>
2021-10-15 22:32:01 +0800

    Modify h1 to button

--- src/Foo/index.tsx
+++ src/Foo/index.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import styles from './index.less';
+import { tuple } from '@/util/type';
+
+function isUnborderedButtonType(type: ButtonType | undefined) {
+  return type === 'text' || type === 'link';
+}
+
+const ButtonTypes = tuple(
+  'default',
+  'primary',
+  'ghost',
+  'dashed',
+  'link',
+  'text',
+);
+export type ButtonType = typeof ButtonTypes[number];
+
+export interface BaseButtonProps {
+  type?: ButtonType;
+}
 
 export default ({ title }: { title: string }) => (
-  <h1 className={styles.title}>{title}</h1>
+  <button className={styles.title}>{title}</button>
 );
