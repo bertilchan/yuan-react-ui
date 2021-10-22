cf55b0717686474aaf487cd6387d72fd40c69518
zhixiangyuan <Goog1e491488323@gmail.com>
2021-10-22 21:39:15 +0800

    Add danager button

--- src/button/index.md
+++ src/button/index.md
@@ -3,6 +3,8 @@
 
 ## Button
 
+### Type
+
 There are `primary` button, `default` button, `dashed` button, `text` button and `link` button in type.
 
 ```tsx
@@ -18,6 +20,32 @@ export default () => (
     <Button type="link">Link Button</Button>
   </>
 );
+```
+
+### Danger
+
+```tsx
+import React from 'react';
+import { Button } from 'yuan-react-ui';
+
+export default () => (
+  <>
+    <Button type="primary" danger>
+      Primary
+    </Button>
+    <Button danger>Default</Button>
+    <Button type="dashed" danger>
+      Dashed
+    </Button>
+    <Button type="text" danger>
+      Text
+    </Button>
+    <Button type="link" danger>
+      Link
+    </Button>
+  </>
+);
+
 ```
 
 More skills for writing demo: https://d.umijs.org/guide/demo-principle
--- src/button/index.tsx
+++ src/button/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './style';
-import { tuple } from '@/util/type';
+import {tuple} from '@/util/type';
 import classNames from 'classnames';
-import { ConfigContext } from '@/config-provider';
+import {ConfigContext} from '@/config-provider';
 
 function isUnborderedButtonType(type: ButtonType | undefined) {
   return type === 'text' || type === 'link';
@@ -23,14 +23,20 @@ export interface BaseButtonProps {
 }
 
 function Button(props: any) {
-  const { loading = false, prefixCls: customizePrefixCls, type } = props;
+  const {
+    loading = false,
+    prefixCls: customizePrefixCls,
+    type,
+    danger
+  } = props;
 
-  let { getPrefixCls } = React.useContext(ConfigContext);
+  let {getPrefixCls} = React.useContext(ConfigContext);
 
   const prefixCls = getPrefixCls('btn', customizePrefixCls);
 
   const classes = classNames(prefixCls, {
     [`${prefixCls}-${type}`]: type,
+    [`${prefixCls}-dangerous`]: !!danger,
   });
 
   const buttonNode = <button className={classes}>ButtonText</button>;
